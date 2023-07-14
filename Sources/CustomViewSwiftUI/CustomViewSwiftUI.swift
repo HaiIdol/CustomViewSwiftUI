6ad0698021abc7d244d7c3b6f411b9c9b91c664e
haiidol <ngochainguyen2601@gmail.com>
2023-07-14 10:27:32 +0700

    init project

---
+++ Sources/CustomViewSwiftUI/CustomViewSwiftUI.swift
@@ -0,0 +1,7 @@
+public struct CustomViewSwiftUI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+        print(text)
+    }
+}
---
+++ Tests/CustomViewSwiftUITests/CustomViewSwiftUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CustomViewSwiftUI
+
+final class CustomViewSwiftUITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CustomViewSwiftUI().text, "Hello, World!")
+    }
+}
